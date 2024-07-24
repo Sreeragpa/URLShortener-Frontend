defc889250064a48f82ae4112104fbae51a8d6ea
Sreerag PA <catchsreerag@gmail.com>
2024-07-24 11:37:18 +0530

    first commit- tailwindcss configured

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:MainComponent}
+];
